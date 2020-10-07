a3cb45d8cc8a7540a10d40f3544b5b98413543bb
Maskelyne <artiom_filatov@list.ru>
2020-10-08 01:11:04 +0300

    РАÐазмÑетка портфолио

--- source/js/modules/portfolio-page-scroll.js
+++ source/js/modules/portfolio-page-scroll.js
@@ -6,17 +6,14 @@
 
   if (portfolioPage) {
     var portfolioBox = document.querySelector('.portfolio-page__box');
+    var portfolioWrapper = document.querySelector('.portfolio-page__wrapper');
 
     document.body.style.overflow = 'hidden';
 
-    window.addEventListener('mousewheel', function () {
-      portfolioPage.style.transition = 'all 0.7s linear';
-      portfolioPage.style.width = 'calc(100% / 1.5)';
-      portfolioPage.style.position = 'fixed';
-      portfolioPage.style.top = '75px';
-      portfolioPage.style.right = '0px';
-      portfolioBox.style.transform = 'translate(-220%, 100%)';
-      portfolioBox.style.transition = 'all 0.7s linear';
+    window.addEventListener('wheel', function () {
+      portfolioWrapper.classList.add('portfolio-page__wrapper--active');
+      portfolioBox.classList.add('portfolio-page__box--active');
+      portfolioPage.classList.add('portfolio-page__company--active');
     });
   }
 
