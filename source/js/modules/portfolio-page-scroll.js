241200854d61ca50f570f2ef9568bdbc52720a54
artemiy <artiom_filatov@list.ru>
2020-10-08 18:00:37 +0300

    Разметка портфолио

--- source/js/modules/portfolio-page-scroll.js
+++ source/js/modules/portfolio-page-scroll.js
@@ -8,12 +8,30 @@
     var portfolioBox = document.querySelector('.portfolio-page__box');
     var portfolioWrapper = document.querySelector('.portfolio-page__wrapper');
 
-    document.body.style.overflow = 'hidden';
-
     window.addEventListener('wheel', function () {
+      portfolioBox.style.transform = 'translate(0%, 10%)';
       portfolioWrapper.classList.add('portfolio-page__wrapper--active');
-      portfolioBox.classList.add('portfolio-page__box--active');
       portfolioPage.classList.add('portfolio-page__company--active');
+
+      function show() {
+        portfolioBox.classList.add('portfolio-page__box--active');
+      }
+
+      setTimeout(show, 500);
+    });
+
+    portfolioPage.addEventListener('scroll', function () {
+      if (portfolioPage.scrollTop === 0) {
+        portfolioBox.style.transform = 'translate(130%, -150%)';
+        portfolioWrapper.classList.remove('portfolio-page__wrapper--active');
+        portfolioPage.classList.remove('portfolio-page__company--active');
+
+        function none() {
+          portfolioBox.classList.remove('portfolio-page__box--active');
+        }
+
+        setTimeout(none, 600);
+      }
     });
   }
 
