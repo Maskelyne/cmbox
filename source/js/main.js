c7e2dbdab909ece90c0577c6368006f48eaa9add
Maskelyne <artiom_filatov@list.ru>
2020-10-19 00:33:11 +0300

    разметка о нас

--- source/js/main.js
+++ source/js/main.js
@@ -8,12 +8,12 @@
 //= modules/header-nav.js
 //= modules/clients-slider.js
 //= modules/validation.js
-//= modules/rotate-card.js
+//= modules/portfolio-rotate-card.js
+//= modules/blog-rotate-card.js
 //= modules/scroll-height.js
 //= modules/sliders.js
 //= modules/bg-main.js
 //= modules/portfolio-page-scroll.js
 //= modules/modals.js
 //= modules/modals-portfolio.js
-//= modules/fullpage.js
 
---
+++ source/js/modules/portfolio-rotate-card.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+
+  var container = document.querySelector('.portfolio');
+
+  if (container) {
+
+    var cards = document.querySelectorAll('.portfolio__box-card');
+
+    function rotate(evt) {
+      var cardItem = this.querySelector('.portfolio__card-perspective');
+      var halfHeight = cardItem.offsetHeight / 2;
+      var halfWidth = cardItem.offsetWidth / 2;
+
+      cardItem.style.transform = 'rotateX(' + -(evt.offsetY - halfHeight) / 7 + 'deg)' +
+        'rotateY(' + (evt.offsetX - halfWidth) / 7 + 'deg';
+    }
+
+    function rotateNone() {
+      var cardItem = this.querySelector('.portfolio__card-perspective');
+
+      cardItem.style.transform = 'rotateX(0deg)' + 'rotateY(0deg)';
+    }
+
+    for (var i = 0; i < cards.length; i++) {
+      var card = cards[i];
+      card.addEventListener('mousemove', rotate);
+      card.addEventListener('mouseout', rotateNone);
+    }
+  }
+
+})();
