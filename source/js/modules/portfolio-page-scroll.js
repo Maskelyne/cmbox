f0e10352cf8d2cd7a5c1a42c79f3914cbd037a5a
Maskelyne <artiom_filatov@list.ru>
2020-11-05 01:12:27 +0300

    ПраÐеревёрстка портфолио

--- source/js/modules/portfolio-page-scroll.js
+++ source/js/modules/portfolio-page-scroll.js
@@ -5,6 +5,7 @@
   var portfolioPage = document.querySelector('.portfolio-page__company');
 
   if (portfolioPage) {
+    var portfolioPageBox = document.querySelector('.portfolio-page');
     var portfolioBox = document.querySelector('.portfolio-page__box');
     var portfolioWrapper = document.querySelector('.portfolio-page__wrapper');
     var portfolioBoxImage = document.querySelector('.portfolio-page__box-image');
@@ -21,8 +22,9 @@
           portfolioWrapper.classList.add('portfolio-page__wrapper--active');
           portfolioPage.classList.add('portfolio-page__company--active');
           portfolioBox.style.transform = 'translate(-54%, 10%)';
-          portfolioBoxImage.style.transform = 'translate(-46%, 0)';
+          portfolioBoxImage.style.transform = 'translate(0%, 0%)';
           setTimeout(show, 400);
+          // setTimeout(grid, 500);
         } else if (evt.deltaY > 0 && window.innerWidth > 1023) {
           portfolioWrapper.classList.add('portfolio-page__wrapper--active');
           portfolioPage.classList.add('portfolio-page__company--active');
@@ -39,10 +41,17 @@
 
         function none() {
           portfolioBox.classList.remove('portfolio-page__box--active');
+          portfolioPageBox.style.display = '';
         }
 
         function show() {
           portfolioBox.classList.add('portfolio-page__box--active');
+        }
+
+        function grid() {
+          portfolioPageBox.style.display = 'grid';
+          portfolioPageBox.style.gridTemplateColumns = '30% auto';
+          portfolioBoxImage.style.transform = 'translate(0%, 0)';
         }
 
       });
