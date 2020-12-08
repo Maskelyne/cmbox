129f8f001e35ed3f8e00be4bd2a0a27b17c1933a
Artem <Matusevich>
2020-12-08 18:01:44 +0300

    Обновление симбокс

--- source/js/main.js
+++ source/js/main.js
@@ -5,11 +5,11 @@
 
 */
 
+//= modules/vendors.js
 //= modules/header-nav.js
 //= modules/clients-slider.js
 //= modules/portfolio-rotate-card.js
 //= modules/scroll-height.js
-//= modules/bg-main.js
 //= modules/portfolio-page-scroll.js
 //= modules/modals.js
 //= modules/modals-portfolio.js
@@ -21,5 +21,4 @@
 //= modules/forms.js
 //= modules/link-hidden.js
 //= modules/feedback-tabs.js
-//= modules/vendors.js
 
